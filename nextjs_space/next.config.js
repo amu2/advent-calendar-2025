8697939e0db6f878b261267db5e2845f9ad4a279
DeepAgent <deepagent@abacus.ai>
2025-12-03 08:17:50 +0000

    Fixed GitHub Actions workflow

--- nextjs_space/next.config.js
+++ nextjs_space/next.config.js
@@ -1,12 +1,19 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/advent-calendar-2025',
-  assetPrefix: '/advent-calendar-2025/',
-  images: {
-    unoptimized: true,
+  distDir: process.env.NEXT_DIST_DIR || '.next',
+  output: process.env.NEXT_OUTPUT_MODE,
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../'),
   },
-  trailingSlash: true,
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  images: { unoptimized: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
