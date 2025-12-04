428f589e9180f17fc4eab86da39e9c6945418f67
DeepAgent <deepagent@abacus.ai>
2025-12-04 18:32:34 +0000

    Fix export config

--- next.config.export.js
+++ next.config.export.js
@@ -3,10 +3,7 @@ const nextConfig = {
   output: 'export',
   basePath: '/advent-calendar-2025',
   assetPrefix: '/advent-calendar-2025/',
-  images: {
-    unoptimized: true,
-  },
   trailingSlash: true,
-}
-
-module.exports = nextConfig
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
